b59c81697785b12fce362b93f76410cfb76bf8b4
Q Xu <xuq3196@outlook.com>
2025-11-04 19:35:24 +0800

    xargs: finish

--- user/xargs.c
+++ user/xargs.c
@@ -2,6 +2,16 @@
 #include "kernel/param.h"
 #include "user/user.h"
 
+void run_command(char *prog_name, char *argv_pass[]) {
+  if (fork() == 0) {
+    exec(prog_name, argv_pass);
+    fprintf(2, "exec error");
+    exit(1);
+  } else {
+    wait(0);
+  }
+}
+
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     fprintf(2, "no enough arg provided\n");
@@ -10,7 +20,7 @@ int main(int argc, char *argv[]) {
 
   char *argv_pass[MAXARG];
   char *prog_name = argv[1];
-  argv_pass[0] = prog_name;
+  argv_pass[0] = prog_name;  // 传入exec的第一参数为程序名
 
   for (int i = 1; i < argc - 1; i++) {
     argv_pass[i] = argv[i + 1];
@@ -19,22 +29,26 @@ int main(int argc, char *argv[]) {
   char buf[512], *p;
   p = buf;
 
-  while (read(0, p++, 1) == 1) {
+  while (read(0, p, 1) == 1) {
     if (*p != '\n') {
+      p++;
       continue;
     }
 
     // 读到了换行符
     *p = 0;  // 添加字符串结束标识
     argv_pass[argc - 1] = buf;
-    if (fork() == 0) {
-      exec(prog_name, argv_pass);
-      fprintf(2, "exec error");
-      exit(1);
-    } else {
-      wait(0);
-      p = buf;
-    }
+    argv_pass[argc] = 0;  // 传入exec的最后一个参数需要为0
+    run_command(prog_name, argv_pass);
+    p = buf;
   }
+
+  if (p != buf) {  // 说明已经读取了字符，但是不是以\n结尾
+    *p = 0;
+    argv_pass[argc - 1] = buf;
+    argv_pass[argc] = 0;
+    run_command(prog_name, argv_pass);
+  }
+
   exit(0);
 }
