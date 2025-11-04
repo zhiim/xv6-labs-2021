1127edebb1030c76f7d3d0460528889b1b7063ce
Q Xu <xuq3196@outlook.com>
2025-11-04 12:36:12 +0800

    xargs: some progress

--- user/xargs.c
+++ user/xargs.c
@@ -11,8 +11,30 @@ int main(int argc, char *argv[]) {
   char *argv_pass[MAXARG];
   char *prog_name = argv[1];
   argv_pass[0] = prog_name;
-  
-  for (int i = 0; i < argc; i++) {
-    argv_pass[i]
+
+  for (int i = 1; i < argc - 1; i++) {
+    argv_pass[i] = argv[i + 1];
   }
+
+  char buf[512], *p;
+  p = buf;
+
+  while (read(0, p++, 1) == 1) {
+    if (*p != '\n') {
+      continue;
+    }
+
+    // 读到了换行符
+    *p = 0;  // 添加字符串结束标识
+    argv_pass[argc - 1] = buf;
+    if (fork() == 0) {
+      exec(prog_name, argv_pass);
+      fprintf(2, "exec error");
+      exit(1);
+    } else {
+      wait(0);
+      p = buf;
+    }
+  }
+  exit(0);
 }
