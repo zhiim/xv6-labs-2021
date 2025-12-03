e3b24d0d2d34de9a856539c2316bc869d979bf39
Q Xu <xuq3196@outlook.com>
2025-12-03 23:44:05 +0800

    complete part 2 pteprintout

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,32 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+// +---------------------------------------------------------+
+// |                       pteprintout                       |
+// +---------------------------------------------------------+
+void
+vmprint_helper(pagetable_t pagetable, int depth)
+{
+  if (depth > 3) {
+    return;
+  }
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if (pte & PTE_V) {
+      for (int j = 0; j < depth - 1; j++) {
+        printf(".. ");
+      }
+      uint64 child = PTE2PA(pte);
+      printf("..%d: pte %p pa %p\n", i, pte, child);
+      vmprint_helper((pagetable_t)child, depth + 1);
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  vmprint_helper(pagetable, 1);
+}
